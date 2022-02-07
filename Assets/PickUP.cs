d05b14fd246b1888dbd9a91953198d2b0d12c492
Rock3tXer <adrian.varga111@gmail.com>
2022-02-07 16:27:00 +0100

    BackUp_1
    Version 0.2 of the game backed up to the github

---
+++ Assets/PickUP.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUP : MonoBehaviour
+{
+    int score = 0;
+    int Key1 = 0;
+    [SerializeField] float destroyTime = 0f;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Pickup")
+        {
+            score = score + 1;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject, destroyTime);
+        }
+        if(other.tag == "Key1")
+        {
+            Key1 = 1;
+            Debug.Log("Key: " + Key1);
+            Destroy(other.gameObject, destroyTime);
+        }
+        
+    }
+    
+    void OnCollisionEnter2D (Collision2D collision)
+   {
+       if (collision.gameObject.tag == "Door1" && Key1 == 1)
+       {
+           Debug.Log("Open the Doors");
+           Destroy(collision.gameObject);
+       }
+       
+       // 
+   }
+}
